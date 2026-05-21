da15c7d1e21d19592a4d9022245fb3e5cfded608
Kristjan <36150130+kristjan-u4@users.noreply.github.com>
2026-05-21 15:56:52 +0300

    Add demo script for weekly run

---
+++ week_8/weekly_demo.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# A shell script to further demonstrate automatization possibilities of UrbanStyle sales data pipeline.
+# In this particular example, we'll be running the pipeline with last week's Monday as start date
+# and last week's Sunday as end date.
+
+# Calculate date range (previous week):
+day_of_week=$(date +%u)
+monday_current_week=$(date -d "today - $(( day_of_week - 1 )) days" +%Y-%m-%d)
+start_date=$(date -d "$monday_current_week - 7 days" +%Y-%m-%d)
+end_date=$(date -d "$monday_current_week - 1 day" +%Y-%m-%d)
+
+current_dir=$(pwd)
+script_dir=$(dirname "$0")
+project_root_dir=$(dirname "$script_dir")
+
+# Regardless of the outcome, return back to where we started after this script terminates.
+trap 'cd "$current_dir"' EXIT
+
+# Go to project root directory:
+cd "$project_root_dir" || exit 1
+
+# Activate Python virtual environment.
+# Unlike in bash, in sh `source` command does not work. Instead of `source`, we need to use a dot.
+. .venv/bin/activate || exit 1
+
+mkdir -p log
+
+# Enables logging of commands below.
+set -x
+
+# Run pipeline:
+python3 week_8/pipeline.py --start-date="$start_date" --end-date="$end_date" >> log/pipeline.log 2>&1
